9d024ee1aad2a95e3d14f084d106bc40c1ea2546
Justin Laureano <gravatron87@gmail.com>
2018-03-03 16:49:52 -0500

    Portfolio site(2017) uploaded

---
+++ gallery/gathering/includes/js/script.js
@@ -0,0 +1,32 @@
+/*
+
+My Custom JS
+============
+
+Author:  Justin Laureano
+Updated: August 2015
+Notes:	 Hand coded for thegathering.com
+
+*/
+
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
+
